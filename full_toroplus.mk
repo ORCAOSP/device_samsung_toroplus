f3788d6fed807cc69443bf53d6aa0b4570d0b3b6
orcaosp <orcaproject@yahoo.com>
2013-04-12 01:12:15 -0400

    initial commit

--- full_toroplus.mk
+++ full_toroplus.mk
@@ -1,4 +1,4 @@
-# Copyright (C) 2012 The Android Open Source Project
+# Copyright (C) 2011 The Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,37 +14,26 @@
 
 #
 # This file is the build configuration for a full Android
-# build for toro hardware. This cleanly combines a set of
+# build for maguro hardware. This cleanly combines a set of
 # device-specific aspects (drivers) with a device-agnostic
 # product configuration (apps). Except for a few implementation
 # details, it only fundamentally contains two inherit-product
-# lines, full and toro, hence its name.
+# lines, full and maguro, hence its name.
 #
 
-# Gallery
-PRODUCT_PACKAGES := \
-    Gallery
-
-#if we do this after the full_base_telephony is included some of these don't get picked up..
-PRODUCT_COPY_FILES += \
-    device/samsung/toroplus/bcmdhd.cal:system/etc/wifi/bcmdhd.cal \
-    device/samsung/toroplus/apns-conf.xml:system/etc/apns-conf.xml \
-    frameworks/native/data/etc/android.hardware.telephony.cdma.xml:system/etc/permissions/android.hardware.telephony.cdma.xml
-
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.cdma.home.operator.numeric=310120 \
-    ro.cdma.home.operator.alpha=Sprint \
-    ro.telephony.default_network=4
+# Get the long list of APNs
+PRODUCT_COPY_FILES := device/samsung/toroplus/test-apns-conf_sprint.xml:system/etc/apns-conf.xml
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # This is where we'd set a backup provider if we had one
 #$(call inherit-product, device/sample/products/backup_overlay.mk)
-# Inherit from toro device
+# Inherit from maguro device
 $(call inherit-product, device/samsung/toroplus/device.mk)
 
 # Set those variables here to overwrite the inherited values.
 PRODUCT_NAME := full_toroplus
 PRODUCT_DEVICE := toroplus
-PRODUCT_BRAND := Samsung
-PRODUCT_MODEL := Galaxy Nexus
+PRODUCT_BRAND := Android
+PRODUCT_MODEL := Full AOSP on Toroplus
+PRODUCT_RESTRICT_VENDOR_FILES := true
